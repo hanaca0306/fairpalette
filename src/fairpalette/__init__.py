85b0c1d6c1bfee34c18717a00fdb8608516db06c
hanaca0306 <168995894+hanaca0306@users.noreply.github.com>
2026-09-18 16:52:53 +0900

    Add Diffusers attribution session

--- src/fairpalette/__init__.py
+++ src/fairpalette/__init__.py
@@ -1,11 +1,14 @@
 """FairPalette attribution manifest primitives."""
 
+from .diffusers import AdapterAttribution, DiffusersAttributionSession
 from .manifest import AttributionManifest, Contributor, ManifestValidationError
 from .royalties import RoyaltyAllocation, calculate_royalties
 
 __all__ = [
+    "AdapterAttribution",
     "AttributionManifest",
     "Contributor",
+    "DiffusersAttributionSession",
     "ManifestValidationError",
     "RoyaltyAllocation",
     "calculate_royalties",
